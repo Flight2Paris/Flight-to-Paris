f2bfe96ad7fde2ed9b9638d26432497c0691fbf3
tmnt <tm@ninjaturtles.org>
2016-05-31 05:15:11 -0700

    typo negors

--- app/view/scoreform.php
+++ app/view/scoreform.php
@@ -1,6 +1,6 @@
 <?php if ( auth::isLoggedIn() ) : ?>
 	<?php if ( $user->fibo > FIBOHI ) : ?>
-		<h1>404 - no se encontraron agujeros negors</h1>
+		<h1>404 - no se encontraron agujeros negros</h1>
 	<?php else : ?>
 		<form action="<?= View::makeUri('/score/') ?>" method="post" >
 		<?php include('captcha.php') ?>
